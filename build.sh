398e4f7623f893d71d6f3c14f0739fe76a0eaa7c
areustle <areustledev@gmail.com>
2017-08-08 15:31:59 -0400

    specify CLHEP, CPPUNIT, FFTW, XERCES libs

--- build.sh
+++ build.sh
@@ -6,6 +6,19 @@ export LDFLAGS="-L${PREFIX}/lib"
 export PYTHON="python2.7"
 export PYTHON_DIR="-I${PREFIX}/include/${PYTHON}"
 export PYTHON_INC="-I${PREFIX}/include/${PYTHON}"
-./configure --prefix=${PREFIX} --exec_prefix=${PREFIX} LDFLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib"
-./hmake EXT_BLD_EXEC_PFX=${PREFIX} HD_EXEC_PFX=${PREFIX} HD_TOP_EXEC_PFX=${PREFIX} PYTHON_INC="-I${PREFIX}/include/${PYTHON}"
-./hmake install HD_EXEC_PFX=${PREFIX} HD_TOP_EXEC_PFX=${PREFIX} PYTHON_INC="-I${PREFIX}/include/${PYTHON}"
+export CLHEP="CLHEP"
+export CPPUNIT="cppunit"
+export FFTW="fftw3"
+export XERCES="xerces-c"
+./configure --prefix=${PREFIX} --exec_prefix=${PREFIX} \
+    LDFLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib"
+./hmake EXT_BLD_EXEC_PFX=${PREFIX} \
+    HD_EXEC_PFX=${PREFIX} \
+    HD_TOP_EXEC_PFX=${PREFIX} \
+    CLHEP=${CLHEP} \
+    CPPUNIT=${CPPUNIT} \
+    FFTW=${FFTW} \
+    XERCES=${XERCES} \
+    PYTHON_INC="-I${PREFIX}/include/${PYTHON}"
+
+./hmake install HD_EXEC_PFX=${PREFIX} HD_TOP_EXEC_PFX=${PREFIX}
