666d566235fb50fab53ff4bb1e8ce76c1cddbc24
Alex Reustle <Areustle@users.noreply.github.com>
2017-08-10 13:35:38 -0400

    More specificied env vars

--- build.sh
+++ build.sh
@@ -15,10 +15,30 @@ export CPPUNIT="cppunit"
 export FFTW="fftw3"
 export XERCES="xerces-c"
 ./configure --prefix=${PREFIX} --exec_prefix=${PREFIX} \
-    LDFLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib"
-./hmake EXT_BLD_EXEC_PFX=${PREFIX} \
+    LDFLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+    EXT_EXEC_PFX=${PREFIX} \
+    EXT_BLD_EXEC_PFX=${PREFIX} \
     HD_EXEC_PFX=${PREFIX} \
     HD_TOP_EXEC_PFX=${PREFIX} \
+    HC_BLD_EXEC_PFX=${PREFIX} \
+    HC_EXEC_PFX=${PREFIX} \
+    CLHEP=${CLHEP} \
+    HOOPS=${HOOPS} \
+    PIL=${PIL} \
+    CFITSIO=${CFITSIO} \
+    READLINE=${READLINE} \
+    PYTHON_INC="-I${PREFIX}/include/${PYTHON}" \
+    CPPUNIT=${CPPUNIT} \
+    FFTW=${FFTW} \
+    XERCES=${XERCES}
+
+./hmake \
+    EXT_EXEC_PFX=${PREFIX} \
+    EXT_BLD_EXEC_PFX=${PREFIX} \
+    HD_EXEC_PFX=${PREFIX} \
+    HD_TOP_EXEC_PFX=${PREFIX} \
+    HC_BLD_EXEC_PFX=${PREFIX} \
+    HC_EXEC_PFX=${PREFIX} \
     CLHEP=${CLHEP} \
     HOOPS=${HOOPS} \
     PIL=${PIL} \
